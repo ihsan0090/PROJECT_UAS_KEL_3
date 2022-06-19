d095e1100a412643c59fc41ead2454d7dd9b68c7
rezkiams <rezkiams06@gmail.com>
2022-06-20 00:45:45 +0700

    menambahkan file views pada application

---
+++ application/views/depan/v_about.php
@@ -0,0 +1,310 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>About</title>
+    <link rel="shorcut icon" href="<?php echo base_url().'theme/images/icon.png'?>">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/bootstrap.min.css'?>">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,700" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/font-awesome.min.css'?>">
+    <!-- Simple Line Font -->
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/simple-line-icons.css'?>">
+    <!-- Owl Carousel -->
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/slick.css'?>">
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/slick-theme.css'?>">
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/owl.carousel.min.css'?>">
+    <!-- Main CSS -->
+    <link href="<?php echo base_url().'theme/css/style.css'?>" rel="stylesheet">
+</head>
+
+<body>
+ 
+    <!--============================= HEADER =============================-->
+    <div class="header-topbar">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-6 col-sm-8 col-md-9">
+                    <div class="header-top_address">
+                        <div class="header-top_list">
+                            <span class="icon-phone"></span>085100257920
+                        </div>
+                        <div class="header-top_list">
+                            <span class="icon-envelope-open"></span>info@smkmetro.sch.id
+                        </div>
+                        <div class="header-top_list">
+                            <span class="icon-location-pin"></span>Jl. Raya Sudirman Kav.55 Jakarta Selatan
+                        </div>
+                    </div>
+                    <div class="header-top_login2">
+                        
+                    </div>
+                </div>
+                <div class="col-xs-6 col-sm-4 col-md-3">
+                    <div class="header-top_login mr-sm-3">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div data-toggle="affix" style="border-bottom:solid 1px #f2f2f2;">
+        <div class="container nav-menu2">
+            <div class="row">
+                <div class="col-md-11">
+                    <nav class="navbar navbar2 navbar-toggleable-md navbar-light bg-faded">
+                        <button class="navbar-toggler navbar-toggler2 navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown">
+                            <span class="icon-menu"></span>
+                        </button>
+                        <a href="<?php echo site_url('');?>" class="navbar-brand nav-brand2"><img class="img img-responsive" width="150px" height="70px" src="<?php echo base_url().'assets/images/logo-up.png'?>"></a>
+                        <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('');?>">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('about');?>">About</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('guru');?>">Guru</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('siswa');?>">Siswa</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('blog');?>">Blog</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('pengumuman');?>">Pengumuman</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('agenda');?>">Agenda</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('download');?>">Download</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('galeri');?>">Gallery</a>
+                                </li>
+                                <li class="nav-item">
+                                  <a class="nav-link" href="<?php echo site_url('contact');?>">Contact</a>
+                                </li>
+                          </ul>
+                    </div>
+                  </nav>
+                </div>
+              </div>
+            </div>
+          </div>
+      <section>
+</section>
+<!--//END HEADER -->
+<!--//END ABOUT IMAGE -->
+<!--============================= WELCOME TITLE =============================-->
+<section class="welcome_about">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-7">
+                <h2>Kata Sambutan</h2>
+                <p>Bismillahirrohmanirrahim
+                    Assalamu’alaikum Warahmatullahi Wabarakatuh,Salam Sejahtera bagi kita semua
+                    Terlebih dahulu kami ucapkan puji dan syukur kehadirat Tuhan Yang Maha Esa, yang telah banyak melimpahkan rahmat dan karuniaNya kepada kami, sehingga kami dapat mewujudkan impian kami untuk turut serta memberikan sumbangsih kami dalam dunia pendidikan khususnya di Indonesia pada umumnya.
+                </p>
+                <p>Pendidikan adalah kunci untuk membuka kemajuan suatu bangsa. Tidak akan maju suatu bangsa jika rakyatnya berada dalam keterbelakangan pendidikan. Sebagai suatu negara yang besar dengan jumlah penduduk yang banyak, negara kita masih sangat memerlukan banyak perubahan positif dalam pendidikan. Pendidikan yang kita perlukan adalah yang tidak hanya memperhatikan kemampuan akademis anak, tetapi pendidikan yang memperhatikan keseimbangan antara kemampuan akademis, kecakapan olah tubuh, enterpreneur, moral baik, yang terbungkus dalam aturan-aturan demi mewujudkan generasi bangsa yang beragama cerdas secara moril dan spirituil.
+                </p>
+                <p>
+                Untuk itu kami merancang pendidikan di SMK METRO ini secara teritegrasi antara kemampuan akademis, olah tubuh, enterpreneur, moralitas yang dijalankan secara bertahap. Kami berharap dengan sistem ini akan dihasilkan anak-anak didik yang cerdas dalam segala aspek serta berakhlak dan bersikap sesuai dengan tuntunan agama dan norma sosial.
+                    Semoga SMK METRO bisa memberikan sumbangsih yang banyak bagi dunia pendidikan Indonesia saat ini dan dimasa yang akan datang. Aamiin… .
+                    Wassalamu’alaikum,Salam Sejahtera bagi kita semua
+                    <p>
+                    Echwal Nur Susanto<p>
+                    (Ketua Yayasan SMK METRO)     
+                </p>
+                </div>
+                <div class="col-md-5">
+                <img src="<?php echo base_url().'assets/images/sss.png'?>" class="img-fluid" alt="#">
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--//END WELCOME TITLE -->
+    <!--============================= WELCOME TITLE =============================-->
+<section class="welcome_about">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-7">
+                <h2>Moto</h2>
+                <p>
+                </p>
+                <p>“Melahirkan Generasi yang berkembang,bermoral baik,dan bermasyarakat”<p>
+ <h3>Berkembang</h3><p>
+1.	Berkemampuan mengikuti perkembangan zaman industri 4.0<p>
+2.	Mampu menguasai bakat dan minat sesuai keahlian siswa<p>
+
+                </p>
+                <p>
+<h3>Moral baik</h3><p>
+1.	Mampu bersosialisasi dengan masyarakat dengan baik<p>
+2.	Beriman dan beramal<p>
+                    <p>
+<h3>Bermasyarakat</h3><p>
+1.	Bersikap benar, jujur, disiplin, tanggung jawab, santun dan ceria, terbuka, dan bekerjasama<p>
+2.	Berpikir logis, kritis, kreatif dan inovatif<p>
+3.	Mengolah dan mengelola keberagaman<p>
+    
+                </p>
+                </div>
+                <div class="col-md-5">
+                    <img src="<?php echo base_url().'theme/images/tarfal.png'?>" class="img-fluid" alt="#">
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--//END WELCOME TITLE -->
+    <!--============================= TESTIMONIAL =============================-->
+    <section class="testimonial">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2>Testimonial</h2>
+                </div>
+                <div class="col-md-12">
+                    <div class="single-item">
+                        <div class="quote">
+                            <i class="fa fa-quote-left" aria-hidden="true"></i>
+                            <p class="quote_text">SMK METRO benar-benar mengagumkan. Saya sangat senang bisa bergabung dengan SMK METRO dan menjadi siswa terbaik tahun 2018.</p>
+                            <div class="testi-img_block">
+                                <img src="<?php echo base_url().'theme/images/student-1.png'?>" class="img-fluid" alt="#">
+                                <p><span>Yanti Yosefa Maharani Saragih</span>Siswa Terbaik 2021</p>
+                            </div>
+                        </div>
+                        <div class="quote">
+                            <i class="fa fa-quote-left" aria-hidden="true"></i>
+                            <p class="quote_text">SMK METRO benar-benar mengagumkan. Saya sangat senang bisa bergabung dengan SMK METRO dan menjadi siswa terbaik tahun 2017. </p>
+                            <div class="testi-img_block">
+                                <img src="<?php echo base_url().'theme/images/student-2.png'?>" class="img-fluid" alt="#">
+                                <p><span>Rezki Amalia Soleha</span>Siswa Terbaik 2022</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--//END TESTIMONIAL -->
+    <!--============================= DETAILED CHART =============================-->
+    <div class="detailed_chart">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-6 col-md-3 chart_bottom">
+                    <div class="chart-img">
+                        <img src="<?php echo base_url().'theme/images/chart-icon_1.png'?>" class="img-fluid" alt="chart_icon">
+                    </div>
+                    <div class="chart-text">
+                        <p><span class="counter"><?php echo $tot_guru;?></span> Guru
+                        </p>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-3 chart_bottom chart_top">
+                    <div class="chart-img">
+                        <img src="<?php echo base_url().'theme/images/chart-icon_2.png'?>" class="img-fluid" alt="chart_icon">
+                    </div>
+                    <div class="chart-text">
+                        <p><span class="counter"><?php echo $tot_siswa;?></span> Siswa
+                        </p>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-3 chart_top">
+                    <div class="chart-img">
+                        <img src="<?php echo base_url().'theme/images/chart-icon_3.png'?>" class="img-fluid" alt="chart_icon">
+                    </div>
+                    <div class="chart-text">
+                        <p><span class="counter"><?php echo $tot_files;?></span> Download
+                        </p>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-3">
+                    <div class="chart-img">
+                        <img src="<?php echo base_url().'theme/images/chart-icon_4.png'?>" class="img-fluid" alt="chart_icon">
+                    </div>
+                    <div class="chart-text">
+                        <p><span class="counter"><?php echo $tot_agenda;?></span> Agenda</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--//END DETAILED CHART -->
+
+        <!--============================= FOOTER =============================-->
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3">
+                        <div class="foot-logo">
+                            <a href="<?php echo site_url();?>">
+                                <img src="<?php echo base_url().'theme/images/logo-white.png'?>" class="img-fluid" alt="footer_logo">
+                            </a>
+                            <p><?php echo date('Y');?> © copyright by SMK METRO</a>. <br>All rights reserved.</p>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="sitemap">
+                                <h3>Menu Utama</h3>
+                                <ul>
+                                    <li><a href="<?php echo site_url();?>">Home</a></li>
+                                    <li><a href="<?php echo site_url('about');?>">About</a></li>
+                                    <li><a href="<?php echo site_url('artikel');?>">Blog </a></li>
+                                    <li><a href="<?php echo site_url('galeri');?>">Gallery</a></li>
+                                    <li><a href="<?php echo site_url('contact');?>">Contact</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                          <div class="sitemap">
+                              <h3>Akademik</h3>
+                              <ul>
+                                  <li><a href="<?php echo site_url('guru');?>">Guru</a></li>
+                                  <li><a href="<?php echo site_url('siswa');?>">Siswa </a></li>
+                                  <li><a href="<?php echo site_url('pengumuman');?>">Pengumuman</a></li>
+                                  <li><a href="<?php echo site_url('agenda');?>">Agenda</a></li>
+                                  <li><a href="<?php echo site_url('download');?>">Download</a></li>
+                              </ul>
+                          </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="address">
+                                <h3>Hubungi Kami</h3>
+                                <p><span>Alamat: </span> Jl. Raya Sudirman Kav.55 Jakarta Selatan</p>
+                                <p>Email : info@smkmetro.sch.id
+                                    <br> Phone : +628510 025 7920</p>
+                                   
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+                <!--//END FOOTER -->
+                <!-- jQuery, Bootstrap JS. -->
+                <script src="<?php echo base_url().'theme/js/jquery.min.js'?>"></script>
+                <script src="<?php echo base_url().'theme/js/tether.min.js'?>"></script>
+                <script src="<?php echo base_url().'theme/js/bootstrap.min.js'?>"></script>
+                <!-- Plugins -->
+                <script src="<?php echo base_url().'theme/js/slick.min.js'?>"></script>
+                <script src="<?php echo base_url().'theme/js/waypoints.min.js'?>"></script>
+                <script src="<?php echo base_url().'theme/js/counterup.min.js'?>"></script>
+                <script src="<?php echo base_url().'theme/js/owl.carousel.min.js'?>"></script>
+                <script src="<?php echo base_url().'theme/js/validate.js'?>"></script>
+                <script src="<?php echo base_url().'theme/js/tweetie.min.js'?>"></script>
+                <!-- Subscribe -->
+                <script src="<?php echo base_url().'theme/js/subscribe.js'?>"></script>
+                <!-- Script JS -->
+                <script src="<?php echo base_url().'theme/js/script.js'?>"></script>
+            </body>
+
+            </html>
