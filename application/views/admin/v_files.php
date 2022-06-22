b315953ed597d240577646a6a2d0c110512beee2
ihsan0090 <haaseeggaw4@gmail.com>
2022-06-23 01:22:41 +0700

    revisi di files

--- application/views/admin/v_files.php
+++ application/views/admin/v_files.php
@@ -10,7 +10,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>MTs Tarbiyatul Falah | Download</title>
+  <title>SMK METRO | Download</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <link rel="shorcut icon" type="text/css" href="<?php echo base_url().'assets/images/favicon.png'?>">
